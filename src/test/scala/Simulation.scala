34bbb1d8f472f392bc83f591a76736d2391d0431
Paul <3755417+halpz@users.noreply.github.com>
2026-05-19 11:01:12 +0100

    Performance test (get ratepayer-properties) (#3)

--- src/test/scala/Simulation.scala
+++ src/test/scala/Simulation.scala
@@ -56,6 +56,12 @@ class Simulation extends PerformanceTestRunner {
       .headers(requestHeaders)
       .check(status.is(200))
 
+  setup("getRatepayerProperties", "Explore Ratepayer Properties call") withRequests
+    http("GetRatepayerProperties")
+      .get(s"$baseUrl/bridge-integration/ratepayer-properties/123456789567")
+      .headers(requestHeaders)
+      .check(status.is(200))
+
   setup("exploreRatepayer", "Explore Ratepayer call") withRequests
     http("ExploreRatepayer")
       .get(s"$baseUrl/bridge-integration/explore-ratepayer/123456789567")
